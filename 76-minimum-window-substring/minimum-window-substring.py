9c241a64d5d8fd26539191162bc146df1461e94d
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-12 01:09:21 -0700

    Time: 336 ms (15.71%) | Memory: 18 MB (98.75%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if(len(t)>len(s)):
+            return ""
+        
+        ans = s+'x'
+        dic = {}
+        for e in t:
+            dic[e] = dic.get(e, 0)+1
+        
+        l = 0
+        for r in range(len(s)):
+            ch = s[r]
+            if(ch in dic):
+                dic[ch]-=1
+            
+            while(max(dic.values())==0):
+                sub = s[l:r+1]
+                if(len(sub)<len(ans)):
+                    ans = sub
+                
+                c = s[l]
+                if(c in dic):
+                    dic[c]+=1
+                l+=1
+        
+        if(len(ans)>len(s)):
+            return ""
+        return ans
