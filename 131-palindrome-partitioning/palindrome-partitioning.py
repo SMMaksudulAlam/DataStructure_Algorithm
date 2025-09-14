51d6f92c3094f7b196b1b67e84b2e22ed75a0baf
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-14 00:29:02 -0700

    Time: 35 ms (94.34%) | Memory: 37 MB (6.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -3,15 +3,19 @@ class Solution:
         ans = []
         def ispal(s):
             return s == s[::-1]
+        dic = {}
         def build(s):
             if(not s):
                 return [[]]
+            if(s in dic):
+                return dic[s]
             ans_comb = []
             for i in range(len(s)):
                 if(ispal(s[:i+1])):
                     ans = build(s[i+1:])
                     for a in ans:
                         ans_comb.append([s[:i+1]] + a)
+            dic[s] = ans_comb
             return ans_comb
 
             
