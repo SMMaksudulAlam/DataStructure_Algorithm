830eab73906ee63a4f42d94886d38fda99e830be
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-15 14:27:47 -0700

    Time: 47 ms (45.59%) | Memory: 17.9 MB (79.62%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -5,26 +5,27 @@ class Solution:
 
         if(len(s1)>len(s2)):
             s1, s2 = s2, s1
+        s1 = 'X'+s1
+        s2 = 'X'+s2
+        s3 = 'X'+s3
         
-        ans = [[False]*(len(s2)+1) for _ in range(len(s1)+1)]
+        ans = [[False]*(len(s2)) for _ in range(len(s1))]
 
-        ans[0][0] = True
-
-        for i in range(1, len(s2)+1):
-            if(s3[i-1]==s2[i-1]):
+        for i in range(len(s2)):
+            if(s3[i]==s2[i]):
                 ans[0][i] = True
             else:
                 break
         
-        for i in range(1, len(s1)+1):
-            if(s3[i-1]==s1[i-1]):
+        for i in range(len(s1)):
+            if(s3[i]==s1[i]):
                 ans[i][0] = True
             else:
                 break
 
-        for i in range(1, len(s1)+1):
-            for j in range(1, len(s2)+1):
-                ans[i][j] = (s3[i+j-1]==s2[j-1] and ans[i][j-1]) or (s3[i+j-1]==s1[i-1] and ans[i-1][j])
+        for i in range(1, len(s1)):
+            for j in range(1, len(s2)):
+                ans[i][j] = (s3[i+j]==s2[j] and ans[i][j-1]) or (s3[i+j]==s1[i] and ans[i-1][j])
         
         #print(s1, s2, ans)
         
