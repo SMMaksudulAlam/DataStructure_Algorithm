1d5e9ae339b3f5fa1d17b28788cd8c2be1cae101
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-15 04:17:00 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.17%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,22 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dir = [(1, 0), (0, 1)]
-        track = set()
-        ans = [[0]*n for _ in range(m)]
-
-        track.add((0, 0))
-        ans[0][0] = 1
-
-        while(track):
-            local_track = set()
-            for x, y in track:
-                for dx, dy in dir:
-                    x_ = x+dx
-                    y_ = y+dy
-                    if(x_<m and y_<n):
-                        ans[x_][y_] += ans[x][y]
-                        local_track.add((x_, y_))
-            track = local_track
+        ans = []
+        ans.append([1]*n)
+        for i in range(m-1):
+            r = [0]*n
+            r[0] = 1
+            ans.append(r)
+        for i in range(1, m):
+            for j in range(1, n):
+                ans[i][j] = ans[i-1][j]+ans[i][j-1]
         return ans[-1][-1]
             
         
