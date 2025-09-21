b68ff94c28d1655d34ef7b585a8d1a0e767d64d9
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-21 03:14:36 -0700

    Time: 590 ms (5.02%) | Memory: 31.1 MB (49.75%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,25 +1,37 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        ans = 0
+        l2r = []
         stack = []
-        for r in range(len(heights)):
-            h = heights[r]
-            l = r
-            while(stack and stack[-1][0]>=h):
-                h_, l_, r_ = stack.pop()
-                area = h_*(r-l_)
-                ans = max(ans, area)
-                l = l_
-            area = h*(r-l+1)
-            ans = max(ans, area)
-            stack.append((h, l, r))
+        for i in range(len(heights)):
+            e = heights[i]
+            while(stack and stack[-1][0]>=e):
+                stack.pop()
+            if(not stack):
+                l2r.append(-1)
+            else:
+                l2r.append(stack[-1][1])
+            stack.append((e, i))
 
-        if(stack):
-            R = stack[-1][2]
-            for (h_, l_, r_) in stack:
-                area = h_*(R-l_+1)
-                ans = max(ans, area)
+        r2l = deque()
+        q = deque()
+        for i in range(len(heights)-1, -1, -1):
+            e = heights[i]
+            while(q and q[0][0]>=e):
+                q.popleft()
+            if(not q):
+                r2l.appendleft(len(heights))
+            else:
+                r2l.appendleft(q[0][1])
+            q.appendleft((e, i))
         
+        ans = 0
+
+        for i in range(len(heights)):
+            h = heights[i]
+            l = i - l2r[i]
+            r = r2l[i] - i
+
+            ans = max(ans, h*(l+r-1))
         return ans
         
 
