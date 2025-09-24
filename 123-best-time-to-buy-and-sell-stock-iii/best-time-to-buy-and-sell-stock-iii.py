3716315d94d6f80b78365dd2d07adbff9f99387a
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-24 04:43:44 -0700

    Time: 209 ms (92.20%) | Memory: 28.7 MB (96.26%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -2,15 +2,14 @@ import heapq as hq
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        buy1, buy2 = inf, inf
-        prof1, prof = 0, 0
+        buy, buy2 = inf, inf
+        prf = 0
 
         for p in prices:
-            buy1 = min(buy1, p)
-            prof1 = max(prof1, p-buy1)
+            buy = min(buy, p)
+            prf = max(prf, p-buy)
 
-            buy2 = min(buy2, p-prof1)
-
-            prof = max(prof, p-buy2)
-
-        return prof
+            buy2 = min(buy2, p-prf)
+            
+            profit = max(profit, p-buy2)
+        return profit
