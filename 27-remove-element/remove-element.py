6bd44b18cfa115985dbd7b5f644eee09053200b8
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-26 00:37:49 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.22%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,16 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         left = 0
-        right = 0
-        while(left<len(nums)):
-            if(nums[left]==val):
-                right = left+1
-                while(right<len(nums)):
-                    if(nums[right]!=val):
-                        nums[left], nums[right] = nums[right], nums[left]
-                        break
-                    right += 1
-                if(right==len(nums)):
-                    return left
-            left+=1
+        for right in range(len(nums)):
+            if(nums[right]!=val):
+                nums[left], nums[right] = nums[right], nums[left]
+                left+=1
         return left
