b1fc09c8501a3c99572485d0314449b5c43d5a33
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-09-26 21:01:49 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (55.22%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
@@ -1,11 +1,11 @@
 class Solution:
     def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        i = 0
+        k = 0
         while(True):
-            rem = num1 - i*num2
-            if(rem<i):
+            rem = num1 - k*num2
+            if(rem<k):
                 return -1
-            if(rem.bit_count()<=i):
-                return i
-            i+=1
+            if(rem.bit_count()<=k):
+                return k
+            k+=1
         return -1
