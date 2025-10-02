e2003577fa1c7e53d0971a8e730ff58e33c8edcd
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-10-02 03:26:08 -0700

    Time: 39 ms (40.69%) | Memory: 17.9 MB (39.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,35 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dic = {}
         if(len(s1)>len(s2)):
             return False
-        
+
+        dic = {}
         for e in s1:
-            dic[e] = dic.get(e, 0) + 1
+            dic[e] = dic.get(e, 0)+1
         
         l = 0
         for r in range(len(s2)):
             if(s2[r] not in dic):
-                while(l<r):
-                    ch = s2[l]
-                    if(ch in dic):
-                        dic[ch]+=1
+                while(l<=r):
+                    if(s2[l] in dic):
+                        dic[s2[l]]+=1
                     l+=1
             else:
-                ch = s2[r]
-                x = dic[ch]
-                x-=1
-                while(dic[ch]<1):
-                    c = s2[l]
-                    if(c in dic):
-                        dic[c]+=1
+                while(dic[s2[r]]<=0):
+                    if(s2[l] in dic):
+                        dic[s2[l]]+=1
                     l+=1
-
-                x = dic[ch]
-                x-=1
-                dic[ch] = x
-            
-            if(max(dic.values())==0):
-                return True
+                dic[s2[r]] -= 1
+                if(max(dic.values())==0):
+                    return True
         return False
-        
+
