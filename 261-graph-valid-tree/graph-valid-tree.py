c6969c179df6c88cd56e26fa34f8ae4f6351a3ff
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-10-02 19:42:20 -0700

    Time: 4 ms (27.30%) | Memory: 18.8 MB (85.27%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,36 +1,46 @@
+class node:
+    def __init__(self, val):
+        self.val = val
+        self.parent = self
+        self.count = 1
+    def find(self):
+        while(self.parent != self):
+            self = self.parent
+        return self
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if(n==1):
-            return True
-        graph = {}
-        for x, y in edges:
-            if(x not in graph):
-                graph[x] = set()
-            if(y not in graph):
-                graph[y] = set()
-            graph[x].add(y)
-            graph[y].add(x)
+        if(len(edges)!=n-1):
+            return False   
+        dic = {}
+        parent = set()
+        for i in range(n):
+            dic[i] = node(i)
+            parent.add(i)
 
-        visited = {}
+        for u, v in edges:
+            u_parent = dic[u].find()
+            v_parent = dic[v].find()
 
-        def dfs(node):
-            if(node in visited):
-                return False
-            visited[node] = 1
-            if(node not in graph):
-                return False
-            for neigh in graph[node]:
-                graph[neigh].remove(node)
-                res = dfs(neigh)
-                if(not res):
-                    return False
-            return True
-        
-        res = dfs(0)
-        if(not res):
+            if(u_parent != v_parent):
+                if(u_parent.count>=v_parent.count):
+                    v_parent.parent = u_parent
+                    u_parent.count += v_parent.count
+                    parent.remove(v_parent.val)
+                else:
+                    u_parent.parent = v_parent
+                    v_parent.count += u_parent.count
+                    parent.remove(u_parent.val)
+
+        if(len(parent)>1):
             return False
         
-        for i in range(n):
-            if(i not in visited):
-                return False
+        prnt = None
+        for p in parent:
+            prnt = p
+
+        print(dic[prnt].count)
+        if(dic[prnt].count!=n):
+            return False
+
         return True
