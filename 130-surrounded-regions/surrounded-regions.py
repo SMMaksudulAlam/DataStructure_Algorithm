b972c40d5c0789db5f2546b553b0dce9210efd6d
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-10-02 19:11:42 -0700

    Time: 51 ms (12.41%) | Memory: 35.1 MB (5.63%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -8,25 +8,23 @@ class Solution:
 
         def dfs(i, j, dic):
             dic[(i, j)] = 1
-            ans = True
+            ans = False
             for dx, dy in dir:
                 x = i+dx
                 y = j+dy
                 if(0<=x<len(board) and 0<=y<len(board[0]) and (x, y) not in dic and board[x][y]=='O'):
                     ans_ = dfs(x, y, dic)
-                    ans = ans and ans_
-            if(not ans):
-                return False
+                    ans = ans or ans_
             if(i==0 or i==len(board)-1 or j==0 or j==len(board[0])-1):
-                return False
-            return True
+                return True
+            return ans
         
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if(board[i][j]=='O'):
                     dic = {}
                     res = dfs(i, j, dic)
-                    if(res):
+                    if(not res):
                         for x, y in dic.keys():
                             board[x][y] = 'X'
                     else:
