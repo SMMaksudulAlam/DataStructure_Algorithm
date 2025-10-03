9642cc4f1a02151163fa71088683e5516e92001f
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2025-10-02 21:39:50 -0700

    Time: 3 ms (68.53%) | Memory: 17.7 MB (87.26%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -3,11 +3,13 @@ class Solution:
         s = 'X'+s
         ans = [0]*(len(s))
         ans[0] = 1
+        
+        wordDict = set(wordDict)
 
         for i in range(1, len(s)):
             for w in wordDict:
                 ln = len(w)
                 if(i-ln>=0 and ans[i-ln]==1):
-                    if(s[i-ln+1:i+1]==w):
+                    if(s[i-ln+1:i+1] in wordDict):
                         ans[i] = 1
         return ans[-1]==1
