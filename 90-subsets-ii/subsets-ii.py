2bd714c9e51413e08f1e2669b3d9e165c3124c7c
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-01-19 15:23:55 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (14.39%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -9,9 +9,7 @@ class Solution:
             for ar in ans:
                 ans_.append(ar)
                 ar_ = ar+[n]
-                s = ""
-                for e in ar_:
-                    s+=str(e)
+                s = ''.join([str(i) for i in ar_])
                 if(s not in dic):
                     dic[s] = 1
                     ans_.append(ar_)
