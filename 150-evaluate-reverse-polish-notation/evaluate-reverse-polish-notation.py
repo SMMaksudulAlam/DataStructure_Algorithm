bbb687be3bf805840f51378a05216041a3871232
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-06-07 21:37:00 -0700

    Time: 3 ms (57.27%) | Memory: 20.8 MB (10.11%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,28 +2,29 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for e in tokens:
-            if(e not in ['+', '-', '*', '/']):
-                stack.append(int(e))
-            else:
-                if(e=='+'):
-                    e1 = stack.pop()
-                    e2 = stack.pop()
-                    stack.append(e2+e1)
-                if(e=='-'):
-                    e1 = stack.pop()
-                    e2 = stack.pop()
-                    stack.append(e2-e1)
-                if(e=='*'):
-                    e1 = stack.pop()
-                    e2 = stack.pop()
-                    stack.append(e2*e1)
-                if(e=='/'):
-                    e1 = stack.pop()
-                    e2 = stack.pop()
-                    div = e2/e1
-                    if(div<0):
-                        stack.append(ceil(div))
+            #print(stack, e)
+            if(e in ["+", "-", "*", "/"]):
+                if(e == "+"):
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op1+op2)
+                if(e == "-"):
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op2-op1)
+                if(e == "*"):
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op1*op2)
+                if(e == "/"):
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    ans = (op2*1.0)/op1
+                    if(ans>=0):  
+                        stack.append(math.floor(ans))
                     else:
-                        stack.append(floor(div))
-        
+                        stack.append(math.ceil(ans))
+            else:
+                stack.append(int(e))
+
         return stack[-1]
