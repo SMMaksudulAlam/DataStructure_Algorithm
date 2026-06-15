c60e557d83e9cef6fa6dab0ced9951329fa79747
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-06-15 00:44:40 -0700

    Time: 546 ms (5.06%) | Memory: 31.9 MB (37.15%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,37 +1,41 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         l2r = []
-        stack = []
-        for i in range(len(heights)):
-            e = heights[i]
-            while(stack and stack[-1][0]>=e):
-                stack.pop()
-            if(not stack):
-                l2r.append(-1)
-            else:
-                l2r.append(stack[-1][1])
-            stack.append((e, i))
+        r2l = deque([])
 
-        r2l = deque()
-        q = deque()
-        for i in range(len(heights)-1, -1, -1):
-            e = heights[i]
-            while(q and q[0][0]>=e):
-                q.popleft()
-            if(not q):
-                r2l.appendleft(len(heights))
-            else:
-                r2l.appendleft(q[0][1])
-            q.appendleft((e, i))
-        
-        ans = 0
+        #going from left to right
+        storage = []
 
-        for i in range(len(heights)):
+        for i, h in enumerate(heights):
+            while(storage and storage[-1][0]>=h):
+                storage.pop()
+
+            if(not storage):
+                l2r.append((-1))
+                storage.append((h, i))
+            else:
+                l2r.append((storage[-1][1]))
+                storage.append((h, i))
+
+        #going from right to left
+        storage = deque([])
+        ln = len(heights)
+        for i in range(ln-1, -1, -1):
             h = heights[i]
-            l = i - l2r[i]
-            r = r2l[i] - i
+            while(storage and storage[0][0]>=h):
+                storage.popleft()
+                
+            if(not storage):
+                r2l.appendleft((ln))
+                storage.appendleft((h, i))
+            else:
+                r2l.appendleft((storage[0][1]))
+                storage.appendleft((h, i))
 
-            ans = max(ans, h*(l+r-1))
+        #print(l2r, r2l)
+        ans = 0
+        for i in range(ln):
+            h = heights[i]
+            w = (i-l2r[i]) + (r2l[i]-i) - 1
+            ans = max(ans, h*w)
         return ans
-        
-
