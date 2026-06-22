5d349b37f9f501d2d3e570d9332da65401bf3e27
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-06-22 10:57:25 -0700

    Time: 338 ms (11.76%) | Memory: 46.2 MB (6.79%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,29 +1,21 @@
 import heapq as hq
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        stack = deque([])
-        for i in range(k):
-            e = nums[i]
-            if(not stack):
-                stack.append((e, i))
-                continue
-            
-            while(stack and stack[-1][0]<=e):
-                stack.pop()
-            
-            stack.append((e, i))
-        
         ans = []
-        ans.append(stack[0][0])
-    
+        h = []
+        for i in range(k):
+            n = nums[i]
+            hq.heappush(h, (-n, i))
+        
+        ans.append(-h[0][0])
         for i in range(k, len(nums)):
-            if(stack[0][1]<=(i-k)):
-                stack.popleft()
-            e = nums[i]
-            while(stack and stack[-1][0]<=e):
-                stack.pop()
-            stack.append((e, i))
-            ans.append(stack[0][0])
-        return ans
+            rmv_ind = i-k
+            while(h and h[0][1]<=rmv_ind):
+                hq.heappop(h)
 
+            n = nums[i]
+            hq.heappush(h, (-n, i))
+            ans.append(-h[0][0])
+        
+        return ans
 
