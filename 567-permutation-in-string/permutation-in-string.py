aeb6f7299bb2ce6412052c47535a9b4fcd038a56
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-06-23 00:46:09 -0700

    Time: 43 ms (33.65%) | Memory: 19.5 MB (18.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -9,24 +9,23 @@ class Solution:
         
         left = 0
         for right in range(len(s2)):
-            if(s2[right] in dic):
-                dic[s2[right]] -= 1
-            
             if(s2[right] not in dic):
                 while(left<right):
                     if(s2[left] in dic):
                         dic[s2[left]] += 1
                     left+=1
-            
-            while(min(dic.values())<0):
-                if(s2[left] in dic):
-                    dic[s2[left]] += 1
-                left+=1
+            else:
+                if(s2[right] in dic):
+                    dic[s2[right]] -= 1
 
-            val = dic.values()
-            if(max(val)==0 and min(val)==0):
+                while(dic[s2[right]]<0):
+                    if(s2[left] in dic):
+                        dic[s2[left]] += 1
+                    left+=1
+
+            if(max(dic.values())==0):
                 return True
-        print(left, right, dic)
+        #print(left, right, dic)
         return False
             
 
