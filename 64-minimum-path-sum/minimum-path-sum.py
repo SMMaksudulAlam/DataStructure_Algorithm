2193632f1cb395feba30e50f1bd73a9531162e38
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-06 11:37:15 -0700

    Time: 43 ms (10.47%) | Memory: 32.1 MB (6.32%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,20 +1,19 @@
 import heapq as hq
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        visited = set()
-        h = []
-        hq.heappush(h, (grid[0][0], 0, 0))
-        visited.add((0, 0))
+        dp = {}
+        row = len(grid)-1
+        col = len(grid[0])-1
 
-        dir = [(1, 0), (0, 1)]
-        while(h):
-            v, x, y = hq.heappop(h)
-            for dx, dy in dir:
-                x_ = x+dx
-                y_ = y+dy
-                if(0<=x_<len(grid) and 0<=y_<len(grid[0]) and (x_, y_) not in visited):
-                    v_ = v + grid[x_][y_]
-                    grid[x_][y_] = v_
-                    visited.add((x_, y_))
-                    hq.heappush(h, (v_, x_, y_))
-        return grid[-1][-1]
+        def traverse(i, j):
+            if((i, j) in dp):
+                return dp[(i, j)]
+            if(i==row and j==col):
+                return grid[i][j]
+            if(i>row or j>col):
+                return inf
+            
+            dp[(i, j)] = grid[i][j] + min(traverse(i+1, j), traverse(i, j+1))
+            return dp[(i, j)]
+        
+        return traverse(0, 0)
