7ad9f94b6b56fac3a5411e840560490f868e9487
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-08 16:12:08 -0700

    Time: 279 ms (89.43%) | Memory: 19.3 MB (92.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        """
         dp = {}
         def LCS(ind1, ind2):
             if((ind1, ind2) in dp):
@@ -17,3 +18,24 @@ class Solution:
 
         ans = LCS(len(text1)-1, len(text2)-1)
         return ans
+        """
+        
+        if(len(text1)>len(text2)):
+            text1, text2 = text2, text1
+
+        ln1 = len(text1)
+        ln2 = len(text2)
+
+        prev = [0]*(ln2+1)
+
+        for ind1 in range(ln1):
+            cur = [0]*(ln2+1)
+            for j in range(1, ln2+1):
+                ind2 = j-1
+                if(text1[ind1] == text2[ind2]):
+                    cur[j] = prev[j-1] + 1
+                else:
+                    cur[j] = max(prev[j-1], prev[j], cur[j-1])
+            prev = cur
+
+        return prev[-1]
