8cf591874530abdd3d9437cde740bcd3f7fc1801
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-10 15:49:36 -0700

    Time: 459 ms (49.04%) | Memory: 159 MB (5.08%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,27 +1,24 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        s = 'X'+s
-        p = 'X'+p
-        l2 = len(s)
-        l1 = len(p)
-
-        ans = []
-        for i in range(l1):
-            row = [False]*l2
-            ans.append(row)
-        ans[0][0] = True
-        for i in range(1, l1):
-            if(p[i]=='*'):
-                ans[i][0] = ans[i-1][0]
-
-        for i in range(1, l1):
-            for j in range(1, l2):
-                if(p[i]==s[j]):
-                    ans[i][j] = ans[i-1][j-1]
-                elif(p[i]=='?'):
-                    ans[i][j] = ans[i-1][j-1]
-                elif(p[i]=='*'):
-                    ans[i][j] = ans[i][j-1] or ans[i-1][j]
-                else:
-                    ans[i][j] = False
-        return ans[-1][-1]
+        dp = {}
+        def match(ind1, ind2):
+            if((ind1, ind2) in dp):
+                return dp[(ind1, ind2)]
+            if(ind1<0 or ind2<0):
+                if(ind1<0 and ind2<0):
+                    return True
+                if(ind1<0 and "*"*(ind2+1) == p[:ind2+1]):
+                    return True
+                return False
+            ans = False
+            if(p[ind2] == '?' or p[ind2] == s[ind1]):
+                ans = ans or match(ind1-1, ind2-1)
+            elif(p[ind2] == '*'):
+                ans = ans or match(ind1-1, ind2) or match(ind1, ind2-1)
+            else:
+                ans = False
+            dp[(ind1, ind2)] = ans
+            return dp[(ind1, ind2)]
+        
+        ans = match(len(s)-1, len(p)-1)
+        return ans
