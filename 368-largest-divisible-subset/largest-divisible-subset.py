90e1e4704556cf263495a36a46d875e945d0400c
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-10 23:46:15 -0700

    Time: 160 ms (90.74%) | Memory: 19.4 MB (80.49%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,22 +1,46 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        """
         nums.sort()
-        dp = [[-1, -1] for i in range (len(nums))]
-        h_count = 0
-        h_ind = 0
-        for i, e in enumerate(nums):
-            dp[i] = [i, 1]
-            for j in range(i-1, -1, -1):
-                if(e%nums[j]==0):
-                    if(dp[j][1]+1>dp[i] [1]):
-                        dp[i] = [j, dp[j][1]+1]
-                        if(dp[i][1]>h_count):
-                            h_count = dp[i][1]
-                            h_ind = i
-        ans = deque()
-        while(dp[h_ind][0]!=h_ind):
-            ans.appendleft(nums[h_ind])
-            h_ind = dp[h_ind][0]
-        ans.appendleft(nums[h_ind])
-        return list(ans)
+        ans = []
+        def lds(ind, ar):
+            nonlocal ans
+            if(ind == len(nums)):
+                if(len(ans)<len(ar)):
+                    ans = ar
+                return
+            n = nums[ind]
+            if(not ar or (n % ar[-1] == 0)):
+                lds(ind+1, ar+[n])
+            lds(ind+1, ar)
+            return 
+        lds(0, [])
+        return ans
+        """
+
+        nums.sort()
+        dp = [1]*len(nums)
+        ans = 0
+        ans_ind = -1
+        track = [-1]*len(nums)
         
+        for ind in range(len(nums)):
+            for prev in range(ind):
+                if(nums[ind]%nums[prev]==0):
+                    if(dp[ind] < dp[prev]+1):
+                        dp[ind] = dp[prev]+1
+                        track[ind] = prev
+                        #print(">>>>>", track, ind, prev)
+            if(ans<dp[ind]):
+                ans = dp[ind]
+                ans_ind = ind
+                
+        lis_array = []
+        #print(nums)
+        #print(dp, track)
+        while(ans_ind != -1):
+            lis_array.append(nums[ans_ind])
+            ans_ind = track[ans_ind]
+            
+        lis_array = lis_array[::-1]
+        return lis_array
