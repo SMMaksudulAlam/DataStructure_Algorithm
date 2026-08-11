14ca126e3552c741fce28cb67b9cadea1c8ac207
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-10 19:21:09 -0700

    Time: 65 ms (5.19%) | Memory: 39.5 MB (5.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
         mxp = prices[0]
         mnp = prices[0]
         profit = 0
@@ -10,5 +11,23 @@ class Solution:
                 mnp = p
             mxp = p
         profit += (mxp-mnp)
+        return profit
+        """
+
+        dp = {}
+        def make_profit(ind, couldBuy):
+            if((ind, couldBuy) in dp):
+                return dp[(ind, couldBuy)]
+            if(ind == len(prices)):
+                return 0
+            profit = 0
+            if(couldBuy==1):
+                profit = max(-prices[ind] + make_profit(ind+1, 0), make_profit(ind+1, 1))
+            else:
+                profit = max(prices[ind] + make_profit(ind+1, 1), make_profit(ind+1, 0))
+            dp[(ind, couldBuy)] = profit
+            return dp[(ind, couldBuy)]
+        
+        profit = make_profit(0, 1)
         return profit
 
