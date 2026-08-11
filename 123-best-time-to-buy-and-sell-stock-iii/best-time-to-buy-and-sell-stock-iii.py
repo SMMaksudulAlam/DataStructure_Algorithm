8cff948075ee3c0efab6d482499a363ccc7d4ef9
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-10 19:37:26 -0700

    Time: 2180 ms (5.03%) | Memory: 611.1 MB (5.00%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,16 +1,23 @@
 import heapq as hq
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = inf
-        profit = 0
-        first_profit = 0
-        second_buy = inf
-
-        for p in prices:
-            buy = min(buy, p)
-            first_profit = max(first_profit, p-buy)
-
-            second_buy = min(second_buy, p-first_profit)
-            profit = max(profit, p-second_buy)
+        dp = {}
+        def make_profit(ind, couldBuy, tx):
+            if((ind, couldBuy, tx) in dp):
+                return dp[(ind, couldBuy, tx)]
+            if(ind == len(prices)):
+                return 0
+            profit = 0
+            if(couldBuy==1):
+                p1 = 0
+                if(tx<2):
+                    p1 = -prices[ind] + make_profit(ind+1, 0, tx+1)
+                    p2 = make_profit(ind+1, 1, tx)
+                    profit = max(p1, p2)
+            else:
+                profit = max(prices[ind] + make_profit(ind+1, 1, tx), make_profit(ind+1, 0, tx))
+            dp[(ind, couldBuy, tx)] = profit
+            return dp[(ind, couldBuy, tx)]
         
+        profit = make_profit(0, 1, 0)
         return profit
