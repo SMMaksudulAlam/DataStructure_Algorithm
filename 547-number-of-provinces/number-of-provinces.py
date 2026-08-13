ec048a2aaac956f4da726918a3d9d7c9b2182e7c
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-12 23:44:29 -0700

    Time: 7 ms (47.71%) | Memory: 21 MB (40.72%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,18 +1,31 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        st = set()
-        def dfs(i):
-            st.add(i)
-            ln = len(isConnected)
-            for j in range(ln):
-                if(isConnected[i][j]==1 and j not in st):
-                    dfs(j)
-            return
-        
-        count = 0
+        visited = set()
+        graph = {}
         ln = len(isConnected)
         for i in range(ln):
-            if(i not in st):
-                count+=1
-                dfs(i)
-        return count
+            for j in range(ln):
+                if(i!=j and isConnected[i][j] == 1):
+                    if(i not in graph):
+                        graph[i] = []
+                    if(j not in graph):
+                        graph[j] = []
+                    graph[i].append(j)
+        #print(graph)
+        ans = 0
+        def traverse_province(i):
+            visited.add(i)
+            neigh = graph.get(i, [])
+            for n in neigh:
+                if(n not in visited):
+                    traverse_province(n)
+            return
+
+        for k in range(ln):
+            if(k not in visited):
+                ans+=1
+                traverse_province(k)
+        return ans
+
+                
+                
