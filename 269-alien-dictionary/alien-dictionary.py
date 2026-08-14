bf1aa518bf4b05994bf0b14b1bf5d639e3204f12
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-14 00:43:46 -0700

    Time: 0 ms (100.00%) | Memory: 20.1 MB (9.30%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,49 +1,63 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        dic = {}
-        for w in words:
-            for ch in w:
-                dic[ch] = set()
+        if(len(words)==1):
+            ans = list(set([ch for ch in words[0]]))
+            return "".join(ans)
 
+
+        graph = {}
         for i in range(len(words)-1):
             w1 = words[i]
             w2 = words[i+1]
 
-            if(len(w1)>len(w2) and w1[:len(w2)]==w2):
+            for ch in w1:
+                if(ch not in graph):
+                    graph[ch] = set()
+            for ch in w2:
+                if(ch not in graph):
+                    graph[ch] = set()
+
+            len1 = len(w1) 
+            len2 = len(w2)
+
+            if(len1>len2 and w1[:len2] == w2):
                 return ""
-            
-            for j in range(len(w1)):
-                if(w1[j]!=w2[j]):
-                    dic[w1[j]].add(w2[j])
+
+            mn_ln = min(len1, len2)
+
+            for j in range(mn_ln):
+                if(w1[j] != w2[j]):
+                    graph[w1[j]].add(w2[j])
                     break
-        
-        track = set()
-        ordered = set()
+            
+        #print(graph)
+
+        completed = set()
         ans = []
+        in_path = set()
 
-        def dfs(ch):                      
-            if(ch in ordered):
-                return True
-            if(ch in track):
+        def traverse(nde):
+            if(nde in in_path):
                 return False
+            if(nde in completed):
+                return True
 
-            track.add(ch)
-            for ch_ in dic[ch]:
-                if(not dfs(ch_)):
+            in_path.add(nde)
+            neigh = graph.get(nde, [])
+
+            for n in neigh:
+                if(not traverse(n)):
                     return False
-                    
-            ordered.add(ch)
-            ans.append(ch)
-            track.remove(ch)
+
+            completed.add(nde)
+            ans.append(nde)
+            in_path.remove(nde)
+
             return True
-        
-        for k in dic.keys():
-            if(k not in ordered):
-                if(not dfs(k)):
-                    return ""
-        s = "".join(ans)
-        return s[::-1]
 
+        for nde in graph.keys():
+            if(not traverse(nde)):
+                return ""
 
-        
-        
+        ans = "".join(ans)
+        return ans[::-1]
