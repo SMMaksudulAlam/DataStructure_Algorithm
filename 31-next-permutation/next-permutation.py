fae51869bfb5c3efda9ae9e8ccb27dee77b2a131
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-16 13:12:13 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.01%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -2,23 +2,33 @@ import heapq as hq
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         h = []
-        ln = len(nums)
-        for i in range(ln-1, -1, -1):
-            n = nums[i]
-            hq.heappush(h, -n)
-            if(-h[0] == n):
-                continue
-            ar = []
-            while(h):
-                if(-h[0]==n and ar and ar[-1] != n):
-                    nums[i] = ar.pop()
-                ar.append(-hq.heappop(h))
-            j = i+1
-            while(ar):
-                nums[j] = ar.pop()
-                j+=1
+        ind = len(nums)-1
+        while(ind>=0):
+            num = nums[ind]
+            hq.heappush(h, -num)
+
+            h_top = -h[0]
+            if(h_top>num):
+                right_ind = len(nums)-1
+                while(h and -h[0]>num):
+                    nums[right_ind] = -hq.heappop(h)
+                    right_ind-=1
+                right_ind+=1
+                nums[ind], nums[right_ind] = nums[right_ind], nums[ind]
+                ind = right_ind
+                break
+            ind-=1
+
+        if(ind<0):
+            left = 0
+            right = len(nums)-1
+            while(left<right):
+                nums[left], nums[right] = nums[right], nums[left]
+                left+=1
+                right-=1
             return
-        for i in range(ln//2):
-            nums[i], nums[ln-1-i] = nums[ln-1-i], nums[i]
+        #print(nums)
+        while(h):
+            nums[ind] = -hq.heappop(h)
+            ind-=1
         return
-        
