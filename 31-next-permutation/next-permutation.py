b6e4ebcb7cede8ff6b946aa6886e70938980c20e
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-16 16:15:34 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.01%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,34 +1,34 @@
 import heapq as hq
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        h = []
-        ind = len(nums)-1
+        ind = len(nums)-2
         while(ind>=0):
-            num = nums[ind]
-            hq.heappush(h, -num)
-
-            h_top = -h[0]
-            if(h_top>num):
-                right_ind = len(nums)-1
-                while(h and -h[0]>num):
-                    nums[right_ind] = -hq.heappop(h)
-                    right_ind-=1
-                right_ind+=1
-                nums[ind], nums[right_ind] = nums[right_ind], nums[ind]
-                ind = right_ind
+            if(nums[ind]<nums[ind+1]):
                 break
             ind-=1
-
+        
         if(ind<0):
             left = 0
             right = len(nums)-1
-            while(left<right):
+            while(left<=right):
                 nums[left], nums[right] = nums[right], nums[left]
                 left+=1
                 right-=1
             return
-        #print(nums)
-        while(h):
-            nums[ind] = -hq.heappop(h)
-            ind-=1
+        
+        ind_right = len(nums)-1
+        while(ind_right>ind):
+            if(nums[ind_right]>nums[ind]):
+                nums[ind_right], nums[ind] = nums[ind], nums[ind_right]
+                break
+            ind_right-=1
+        
+        left = ind+1
+        right = len(nums)-1
+        while(left<=right):
+            nums[left], nums[right] = nums[right], nums[left]
+            left+=1
+            right-=1
         return
+        
+
