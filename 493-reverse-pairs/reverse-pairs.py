35bab3e50cd2aad5649941b8dbada041e39db841
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-17 01:46:40 -0700

    Time: 900 ms (48.45%) | Memory: 28.8 MB (48.02%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -1,56 +1,58 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        count = 0
-        def merge(left, right):
-            nonlocal count
-            temp = []
-            l = len(left)
-            r = len(right)
-            #print(left, right)
-            il = 0
-            ir = 0
+        ans = 0
+        def merge(left, right): #[[1, 8, 18, 24], [3, 7, 10]
+            nonlocal ans
+            l_len = len(left)
+            r_len = len(right)
 
-            while(il<l and ir<r):
-                a = left[il]
-                b = right[ir]
-                
-                if(a<=b):
-                    temp.append(a)
-                    il+=1
+            l_i = 0
+            r_i = 0
+            temp_ans = 0
+
+            while(l_i<l_len and r_i<r_len):
+                l_e = left[l_i]
+                r_e = right[r_i]
+                if(l_e <= 2*r_e):
+                    temp_ans += r_i #3
+                    l_i += 1
                 else:
-                    ind = bisect.bisect_right(left, 2*b)
-                    count += (l-ind)
-                    #print(f'for {b}, count increased: {l-ind}')
-                    temp.append(b)
-                    ir+=1
+                    while(r_i<r_len and l_e > 2*right[r_i]):
+                        r_i += 1
+            if(l_i<l_len):
+                temp_ans += ((l_len-l_i)*r_len) #6
+            
+            ans += temp_ans
+            ar = [] #[1, 8, 18, 24], [3, 7, 10]
+            l_i = 0
+            r_i = 0
+            while(l_i<l_len or r_i<r_len): 
+                l_e = left[l_i] if l_i<l_len  else inf
+                r_e = right[r_i] if r_i<r_len else inf
 
-            if(il<l):
-                temp+=left[il:]
-            if(ir<r):
-                for b in right[ir:]:
-                    ind = bisect.bisect_right(left, 2*b)
-                    count += (l-ind)
-                    #print(f'for {b}, count increased: {l-ind}')
-                    temp.append(b)
+                if(l_e <= r_e):
+                    ar.append(l_e)
+                    l_i += 1
+                else:
+                    ar.append(r_e)
+                    r_i += 1
+            return ar
 
-            #print(f'finally,  {temp}, {count}')
-            return temp
-        
-        def split(lst):
+        #ar = merge([1, 8, 18, 24], [3, 7, 10])
+        #print(ans, ar)
+
+        def m_sort(lst):
             if(len(lst)==1):
                 return lst
             mid = len(lst)//2
-
             left = lst[:mid]
             right = lst[mid:]
 
-            left = split(left)
-            right = split(right)
+            left = m_sort(left)
+            right = m_sort(right)
 
-            merged = merge(left, right)
-            return merged
-        
-        split(nums)
-        return count
+            lst = merge(left, right)
+            return lst
 
-
+        nums = m_sort(nums)
+        return ans 
