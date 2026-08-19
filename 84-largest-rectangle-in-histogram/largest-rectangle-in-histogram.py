8cdc1428092a873b93c7b091ade7f6f3f2f99a62
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-18 17:54:46 -0700

    Time: 1021 ms (5.19%) | Memory: 40.4 MB (5.48%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,41 +1,33 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        l2r = []
-        r2l = deque([])
-
-        #going from left to right
-        storage = []
-
-        for i, h in enumerate(heights):
-            while(storage and storage[-1][0]>=h):
-                storage.pop()
-
-            if(not storage):
-                l2r.append((-1))
-                storage.append((h, i))
+        q = deque([])
+        min_right = deque([])
+        for i in range(len(heights)-1, -1, -1):
+            n = heights[i]
+            while(q and q[0][0]>=n):
+                q.popleft()
+            if(not q):
+                min_right.appendleft(len(heights))
             else:
-                l2r.append((storage[-1][1]))
-                storage.append((h, i))
+                min_right.appendleft(q[0][1])
+            q.appendleft((n, i))
 
-        #going from right to left
-        storage = deque([])
-        ln = len(heights)
-        for i in range(ln-1, -1, -1):
-            h = heights[i]
-            while(storage and storage[0][0]>=h):
-                storage.popleft()
-                
-            if(not storage):
-                r2l.appendleft((ln))
-                storage.appendleft((h, i))
+        q = deque([])
+        min_left = deque([])
+        for i in range(len(heights)):
+            n = heights[i]
+            while(q and q[-1][0]>n):
+                q.pop()
+            if(not q):
+                min_left.append(-1)
             else:
-                r2l.appendleft((storage[0][1]))
-                storage.appendleft((h, i))
+                min_left.append(q[-1][1])
+            q.append((n, i))
 
-        #print(l2r, r2l)
-        ans = 0
-        for i in range(ln):
-            h = heights[i]
-            w = (i-l2r[i]) + (r2l[i]-i) - 1
-            ans = max(ans, h*w)
-        return ans
+        max_area = 0
+
+        for i in range(len(heights)):
+            left = (i - min_left[i])
+            right = (min_right[i]-i)
+            max_area = max(max_area, (heights[i]*(left+right-1)))
+        return max_area
