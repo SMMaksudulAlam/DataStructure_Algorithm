db2d6f65aa75538cd5cba378448a85b8a809c796
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-23 01:13:12 -0700

    Time: 3 ms (22.41%) | Memory: 19.4 MB (63.94%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -3,13 +3,12 @@ class Solution:
         nums = [str(i) for i in range(1, n+1)]
         facto = 1
         for i in range(1, n):
-            facto*=i # 6
+            facto*=i
         
         ans = ""
         last = n-1
         while(facto>1):
             ind = (k-1)//facto
-            #print(nums, k, facto, last, ind)
             num = nums[ind]
             ans += str(num) 
             nums.remove(num)
@@ -17,7 +16,6 @@ class Solution:
             facto = facto//last if last else 1
             last -= 1 if last > 0 else 1
 
-        #print(facto, k, nums, ans)
         if(k<=1):
             ans += ''.join(nums)
         else:
