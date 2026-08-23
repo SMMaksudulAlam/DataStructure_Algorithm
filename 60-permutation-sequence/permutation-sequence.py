2f56a682f91dcd0d7ea96aa1397341d04864d9d5
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-23 14:58:47 -0700

    Time: 3 ms (22.39%) | Memory: 19.3 MB (88.45%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -1,25 +1,17 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        nums = [str(i) for i in range(1, n+1)]
-        facto = 1
+        nums = [str(i) for i in range(1, n+1)] #[1, 2, 3, 4]
+        block_size = 1
         for i in range(1, n):
-            facto*=i
-        
+            block_size *= i #6
+
         ans = ""
-        last = n-1
-        while(facto>1):
-            ind = (k-1)//facto
-            num = nums[ind]
-            ans += str(num) 
-            nums.remove(num)
-            k -= (ind)*facto 
-            facto = facto//last if last else 1
-            last -= 1 if last > 0 else 1
+        while(nums):
+            ind = (k-1)//block_size #1 1 0 0 #(k-1) is very important here.
+            num = nums[ind] #2 3 1 4
+            ans += num #2314
 
-        if(k<=1):
-            ans += ''.join(nums)
-        else:
-            ans += ''.join(nums[::-1])
+            k -=  (ind)*block_size #3 1 1 1
+            block_size = block_size//(len(nums)-1) if (len(nums)-1 > 0) else 1 #2 1 1 1
+            nums.remove(num) #[1, 3, 4] [1, 4] [4] []
         return ans
-
-
