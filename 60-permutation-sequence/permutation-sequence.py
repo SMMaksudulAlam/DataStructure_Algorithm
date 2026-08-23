28767cd76e0c18e1614db10d1886a5d3057a0a68
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-23 01:12:58 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.94%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -1,28 +1,27 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        def fact(n):
-            ans = 1
-            for i in range(1, n+1):
-                ans*=i
-            return ans
-        ar = [i for i in range(1, n+1)]
+        nums = [str(i) for i in range(1, n+1)]
+        facto = 1
+        for i in range(1, n):
+            facto*=i # 6
+        
+        ans = ""
+        last = n-1
+        while(facto>1):
+            ind = (k-1)//facto
+            #print(nums, k, facto, last, ind)
+            num = nums[ind]
+            ans += str(num) 
+            nums.remove(num)
+            k -= (ind)*facto 
+            facto = facto//last if last else 1
+            last -= 1 if last > 0 else 1
 
-        f = fact(n+1) #24 #6 #2
-        def build(n, k):#4, 9
-            s = ""
-            nonlocal f
-            while(n>0):
-                f = f//(n+1)
-                seg_size = f//n #6 #2 #1
-                ind = k//seg_size #1 #1 
-                if(k%seg_size==0):
-                    ind -= 1     
-                s+=str(ar[ind]) #2 #3
-                ar.remove(ar[ind]) #[1,3,4] #[1, 4]
-                n-=1 #3 #2
-                k = k%seg_size #3 #1
-            return s
+        #print(facto, k, nums, ans)
+        if(k<=1):
+            ans += ''.join(nums)
+        else:
+            ans += ''.join(nums[::-1])
+        return ans
 
-        s = build(n, k)
-        return s
 
