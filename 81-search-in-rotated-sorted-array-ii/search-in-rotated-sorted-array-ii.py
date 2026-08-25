10dc7f68a589f28b7309145ff60801e93446e507
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-25 12:17:05 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (10.14%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -8,21 +8,20 @@ class Solution:
 
         while(left<=right):
             mid = (left+right)//2
-            if(nums[mid]==target or nums[left]==target or nums[right]==target):
+            if(nums[mid]==target):
                 return True
-            
-            if(nums[left]<=nums[mid]<=nums[right]):
+            if(nums[left]==nums[mid] and nums[mid]==nums[right]):
                 left+=1
                 right-=1
                 continue
             
-            if(nums[left]>nums[mid]):
-                if(target>nums[left] or target<nums[mid]):
-                    right = mid-1
+            if(nums[left]<=nums[mid]):
+                if(nums[left]<=target<nums[mid]):
+                    right = mid - 1
                 else:
-                    left = mid+1
+                    left = mid + 1
             else:
-                if(target>nums[mid] or target<nums[right]):
+                if(nums[mid]<target<=nums[right]):
                     left = mid+1
                 else:
                     right = mid-1
