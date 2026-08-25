cf44160eaa89f20e332bdec3b5c127c627cab437
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-25 12:29:08 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (38.91%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,34 +4,19 @@ class Solution:
         right = len(nums)-1
 
         while(left<=right):
-            left_e = nums[left]
-            right_e = nums[right]
-
-            if(left==right):
-                if(left_e == target):
-                    return left
-                else:
-                    return -1
-
-            if(left+1 == right):
-                if(left_e == target):
-                    return left
-                elif(right_e == target):
-                    return right
-                else:
-                    return -1 
-
             mid = (left+right)//2
-            mid_e = nums[mid]
 
-            if(left_e<mid_e):
-                if(left_e<=target<=mid_e):
-                    right = mid
+            if(nums[mid]==target):
+                return mid
+            
+            if(nums[left]<=nums[mid]):
+                if(nums[left]<=target<nums[mid]):
+                    right = mid-1
                 else:
-                    left = mid
+                    left = mid+1
             else:
-                if(mid_e<=target<=right_e):
-                    left = mid
+                if(nums[mid]<target<=nums[right]):
+                    left = mid+1
                 else:
-                    right = mid
+                    right = mid-1
         return -1
