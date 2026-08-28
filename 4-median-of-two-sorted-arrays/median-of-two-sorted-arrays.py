dd011bba7531662161fdde42d65a8aa7c354d530
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-27 19:23:41 -0700

    Time: 2 ms (51.56%) | Memory: 19.6 MB (14.96%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,38 +1,52 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        len1 = len(nums1)
-        len2 = len(nums2)
-
-        if(len1>len2):
-            len1, len2 = len2, len1
-            nums1, nums2 = nums2, nums1
-        
-        total_count = len1+len2
-        left_part_count = (total_count // 2)
-
-        left1 = 0
-        right1 = len1 - 1
-
-        while(True):
-            mid1 = (left1 + right1) // 2
-            mid_e11 = nums1[mid1] if(nums1 and mid1>=0) else -inf
-            mid_e12 = nums1[mid1+1] if(nums1 and mid1+1<len1) else inf
-
-            rest = left_part_count - (mid1 + 1)
-            mid2 = rest - 1
-            mid_e21 = nums2[mid2] if(nums2 and mid2>=0) else -inf
-            mid_e22 = nums2[mid2+1] if(nums2 and mid2+1<len2) else inf
-
-            if(mid_e11 <= mid_e22 and mid_e21 <= mid_e12):
-                #print(mid_e11, mid_e12, ">>>", mid_e21, mid_e22)
-                if(total_count%2==1):
-                    return min(mid_e12, mid_e22)
-                else:
-                    return (min(mid_e12, mid_e22) + max(mid_e11, mid_e21))/2
+        a = nums1
+        b = nums2
+        k = (len(a) + len(b) +1)//2
             
-            if(mid_e11 > mid_e22):
-                right1 = mid1-1
+        if(len(a)>len(b)):
+            a, b = b, a
+        
+        len_a = len(a)
+        len_b = len(b)
+        
+        left = -1
+        right = len_a
+        
+        while(left<=right):
+            mid = (left+right)//2
+            left_a = a[mid] if mid>=0 else -math.inf
+            right_a = a[mid+1] if mid+1 < len_a else math.inf
+            
+            rest = k-(mid+1)
+            
+            b_ind = rest-1
+            left_b = None
+            if(0<=b_ind<len_b):
+                left_b = b[b_ind]
+            elif(b_ind>=len_b):
+                left_b = math.inf
             else:
-                left1 = mid1+1
+                left_b = -math.inf
+            
+            right_b = None
+            if(0<=b_ind+1<len_b):
+                right_b = b[b_ind+1]
+            elif(b_ind+1>=len_b):
+                right_b = math.inf
+            else:
+                right_b = -math.inf
+            
+            if(left_a<=right_b and left_b<=right_a):
+                #print(left_a, right_a, left_b, right_b)
+                if((len(a) + len(b))%2==1):
+                    return max(left_a, left_b)
+                else: 
+                    return (max(left_a, left_b) + min(right_a, right_b))/2.0
 
+            if(left_a>right_b):
+                right = mid-1
+            else:
+                left = mid+1
+        
         return -1
