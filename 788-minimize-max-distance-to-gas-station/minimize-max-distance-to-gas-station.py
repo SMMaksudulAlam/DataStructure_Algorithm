e6fce14048a49920372962fa11c10ba294ab23e1
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-27 17:45:43 -0700

    Time: 159 ms (91.43%) | Memory: 19.7 MB (54.29%) - LeetSync

--- 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.py
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.py
@@ -1,32 +1,58 @@
+import heapq as hq
 class Solution:
     def minmaxGasDist(self, stations: List[int], k: int) -> float:
-        def cover_station(dis):
-            nonlocal k
-            used_k = 0
-            for i in range(1, len(stations)):
-                gap = stations[i] - stations[i-1]
-                used_k += (ceil(gap/dis) - 1)
-                if(used_k>k):
-                    return False
-            return True
+        """
+        dist = []
+        for i in range(1, len(stations)):
+            dis = stations[i]-stations[i-1]
+            hq.heappush(dist, (-dis, (dis, 1)))
+        
+        for i in range(k):
+            split, (dis, count) = hq.heappop(dist)
+            split = dis/((count+1)*1.0)
+            hq.heappush(dist, (-split, (dis, count+1)))
+
+        split, (dis, count) = dist[0]
+        return -split
+        """
 
 
-        stations.sort()
+        """
+        def count_new_stations(max_dis):
+            count = 0
+            last_pos = stations[0]
+            i = 1
+            while(last_pos<stations[-1]):
+                if(last_pos + max_dis < stations[i]):
+                    count+=1
+                    last_pos += max_dis
+                else:
+                    last_pos = stations[i]
+                    i+=1
+            return count
+        """
+
+        gaps = [stations[i]-stations[i-1] for i in range(1, len(stations))]
+        
+        def count_new_stations(max_dis):
+            count = 0
+            for gap in gaps:
+                count += (math.ceil(gap/max_dis) - 1)
+            return count
 
         left = 0
-        right = stations[-1]
+        right = 0
+        for i in range(1, len(stations)):
+            right = max(right, stations[i]-stations[i-1])
         
-        while(left<=right):
-            #print(left, right)
-            if(right-left<=0.000001):
-                return left
-            
+        ans = 0
+        threshold = 10**(-6)
+        while(right-left>threshold):
             mid = (left+right)/2.0
-            #print('for mid:', mid, cover_station(mid), left, right)
-            if(cover_station(mid)):
+            count = count_new_stations(mid)
+            if(count<=k):
+                ans = mid
                 right = mid
             else:
                 left = mid
-        return -1
-
-                
+        return ans
