7190f3e061ba575c8470c7cb2548591284b866ee
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-29 02:43:25 -0700

    Time: 44 ms (12.54%) | Memory: 20.3 MB (62.38%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,5 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        """
         ans = [False]*len(nums)
         ans[0] = True
         for i, e in enumerate(nums):
@@ -10,3 +11,22 @@ class Solution:
             if(ans[-1] == True):
                 return True
         return False
+        """
+        max_reach = 0
+        curr = 0
+        next_iteration = True
+        while(next_iteration):
+            next_iteration = False
+            next_max_reach = max_reach
+            while(curr<=max_reach):
+                if(curr>=len(nums)-1):
+                    return True
+                next_max_reach = max(next_max_reach, curr + nums[curr])
+                curr+=1
+            if(next_max_reach>max_reach):
+                next_iteration = True
+                max_reach = next_max_reach
+        return False
+
+
+        
