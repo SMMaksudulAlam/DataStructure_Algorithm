3eedaa28bdb20a19697f6a55bdc54c70a04daabf
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-28 20:23:29 -0700

    Time: 135 ms (24.44%) | Memory: 19.8 MB (37.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -7,12 +7,15 @@ class Solution:
         left = 0
         found = False
         ans = s
+        counter_zero_feq = 0
         for right in range(len(s)):
             ch = s[right]
             if(ch in dic):
                 dic[ch] -= 1
+                if(dic[ch] == 0):
+                    counter_zero_feq += 1
             
-            while(max(dic.values())<=0):
+            while(counter_zero_feq == len(dic.keys())):
                 length = right - left + 1
                 found = True
                 if(len(ans)>length):
@@ -20,6 +23,8 @@ class Solution:
                 ch = s[left]
                 if(ch in dic):
                     dic[ch] += 1
+                    if(dic[ch] == 1):
+                        counter_zero_feq -= 1
                 left+=1
 
         if(found):
