7c7be0559c05184ba883df872eceec3ff660027b
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-29 21:00:56 -0700

    Time: 90 ms (5.08%) | Memory: 34.4 MB (5.01%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,27 +1,26 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        open = []
-        star = []
-        for i, e in enumerate(s):
-            if(e=='('):
-                open.append(i)
-            elif(e=='*'):
-                star.append(i)
-            else:
-                if(not open and not star):
-                    return False
-                if(open):
-                    open.pop()
-                else:
-                    star.pop()
-        if(len(open)>len(star)):
-            return False
-        
-        while(open and star):
-            if(open[-1]>star[-1]):
+        dp = {}
+        def check(left, right, ind):
+            if(right>left):
                 return False
-            open.pop()
-            star.pop()
+            if(ind>=len(s)):
+                if(left == right):
+                    return True
+                return False
+            if((left, right, ind) in dp):
+                return dp[(left, right, ind)]
+            ans = False
+            if(s[ind] == '('):
+                ans = ans or check(left+1, right, ind+1)
+            elif(s[ind] == ')'):
+                if(right<left):
+                    ans = ans or check(left, right+1, ind+1)
+            else:
+                ans = ans or check(left+1, right, ind+1) or check(left, right+1, ind+1) or check(left, right, ind+1)
+            
+            dp[(left, right, ind)] = ans
+            return ans
         
-        return True
-
+        ans = check(0, 0, 0)
+        return ans
