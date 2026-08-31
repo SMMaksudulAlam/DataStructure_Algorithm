0efbb741dff9e2bc6aec7b0c05b92aa219d52a7a
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-08-31 14:55:38 -0700

    Time: 4 ms (15.29%) | Memory: 20.6 MB (8.71%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.py
@@ -6,6 +6,53 @@
 #         self.right = right
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        # this is the code on assumption of left/right most nodes of each level are on left/right boundary.
+        left_level = set()
+        left = []
+
+        def traverse_left(root, level):
+            if(not root):
+                return
+            if(root.left == None and root.right == None):
+                left_level.add(level)
+                left.append(root.val)
+                return
+            
+            if(level not in left_level):
+                left_level.add(level)
+                left.append(root.val)
+
+            traverse_left(root.left, level+1)
+            traverse_left(root.right, level+1)
+
+            return
+        
+        right_level = set()
+        right = []
+
+        def traverse_right(root, level):
+            if(not root):
+                return
+            if(root.left == None and root.right == None):
+                right_level.add(level)
+                right.append(root.val)
+                return
+            
+            if(level not in right_level):
+                right_level.add(level)
+                right.append(root.val)
+
+            traverse_right(root.right, level+1)
+            traverse_right(root.left, level+1)
+
+            return
+        
+        traverse_left(root.left, 1)
+        traverse_right(root.right, 1)
+        return [root.val] + left + right[::-1]
+        """
+
         # definition of boundary in the question is very very very important. assuming the left/right most node on left/right boundary is not aligned to the definition
         left = []
         def traverse_left(root, on_left_boundary):
