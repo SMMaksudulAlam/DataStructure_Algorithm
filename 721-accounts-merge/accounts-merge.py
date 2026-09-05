6de565f67aff2f2bcf12d1922cd28b8bc26c6e98
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-05 14:47:17 -0700

    Time: 143 ms (6.94%) | Memory: 32.3 MB (5.10%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,61 +1,55 @@
 import heapq as hq
-
 class uf:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.emails = set()
+        self.emails.add(email)
         self.parent = self
-        self.childs = set()
-        self.childs.add(id)
-
+    
     def find(self):
-        while(self != self.parent):
-            self = self.parent
-        return self
+        cur = self
+        while(cur.parent != cur):
+            cur = cur.parent
+        parent = cur
+        return parent
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        dic = {}
-        parent = set()
-        emails = {}
-
-        for acc_no in range(len(accounts)):
-            acc = accounts[acc_no]
-            dic[acc_no] = uf(acc_no)
-            parent.add(acc_no)
-
+        accs = {}
+        parents = set()
+        for acc in accounts:
+            name = acc[0]
             for i in range(1, len(acc)):
-                eml = acc[i]
-                if(eml in emails):
-                    another_acc = dic[emails[eml]]
-                    another_acc_parent = another_acc.find()
-                    my_acc_parent = dic[acc_no].find()
+                email = acc[i]
+                nde = uf(name, email)
+                accs[email] = nde
+                parents.add(email)
+        #print(accs, parents)
 
-                    if(another_acc_parent == my_acc_parent):
-                        continue
-
-                    if(len(another_acc_parent.childs)>len(my_acc_parent.childs)):
-                        my_acc_parent.parent = another_acc_parent
-                        parent.remove(my_acc_parent.id)
-                        for child in my_acc_parent.childs:
-                            another_acc_parent.childs.add(child)
+        for acc in accounts:
+            email1 = acc[1]
+            for i in range(2, len(acc)):
+                email2 = acc[i]
+                pr1 = accs[email1].find()
+                pr2 = accs[email2].find()
+                if(pr1 != pr2):
+                    if(len(pr1.emails) >= len(pr2.emails)):
+                        pr1.emails = pr1.emails.union(pr2.emails)
+                        pr2.parent = pr1
+                        parents.remove(pr2.email)
                     else:
-                        another_acc_parent.parent = my_acc_parent.parent
-                        parent.remove(another_acc_parent.id)
-                        for child in another_acc_parent.childs:
-                            my_acc_parent.childs.add(child)
-
-                emails[eml] = acc_no
-        
+                        pr2.emails = pr1.emails.union(pr2.emails)
+                        pr1.parent = pr2
+                        parents.remove(pr1.email)
+            
         ans = []
-        for p in parent:
-            acc = dic[p]
-            name = accounts[p][0]
-            emails = set()
-            for ch in acc.childs:
-                for eml in accounts[ch][1:]:
-                    emails.add(eml)
-            emails = list(emails)
+        for p in parents:
+            pr = accs[p]
+            acc = [pr.name]
+            emails = list(pr.emails)
             emails.sort()
-            lst = [name] + emails
-            ans.append(lst)
+            acc += emails
+            ans.append(acc)
         return ans
+
