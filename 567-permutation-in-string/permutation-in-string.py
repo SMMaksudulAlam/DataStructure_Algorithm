3f46772c8a9f711015834f6c63597c320d854153
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-10 18:16:33 -0700

    Time: 19 ms (59.82%) | Memory: 19.6 MB (19.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,30 +3,29 @@ class Solution:
         if(len(s1)>len(s2)):
             return False
         
-        dic = {}
-        for e in s1:
-            dic[e] = dic.get(e, 0)+1
+        track_s1 = [0]*26
+        for ch in s1:
+            ind = ord(ch) - ord('a')
+            track_s1[ind]+=1
         
+        track_s2 = [0]*26
         left = 0
-        for right in range(len(s2)):
-            if(s2[right] not in dic):
-                while(left<right):
-                    if(s2[left] in dic):
-                        dic[s2[left]] += 1
-                    left+=1
-            else:
-                if(s2[right] in dic):
-                    dic[s2[right]] -= 1
-
-                while(dic[s2[right]]<0):
-                    if(s2[left] in dic):
-                        dic[s2[left]] += 1
-                    left+=1
-
-            if(max(dic.values())==0):
-                return True
-        #print(left, right, dic)
-        return False
-            
-
         
+        for right in range(len(s2)):
+            ch = s2[right]
+            ind = ord(ch) - ord('a')
+            track_s2[ind] += 1
+            while(track_s2[ind] > track_s1[ind]):
+                ch_ = s2[left]
+                ind_ = ord(ch_) - ord('a')
+                track_s2[ind_] -= 1
+                left += 1
+            
+            matched = True
+            for ind in range(26):
+                if(track_s2[ind] != track_s1[ind]):
+                    matched = False
+                    break
+            if(matched):
+                return True
+        return False
