e97cdd8e47a300568625c9d21d58c2c6f84a4c52
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-11 22:56:03 -0700

    Time: 211 ms (10.47%) | Memory: 21 MB (15.99%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,32 +1,29 @@
 import heapq as hq
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        dic = {}
-        for t in tasks:
-            dic[t] = dic.get(t, 0)+1
-        
-        h = []
-        for k, v in dic.items():
-            h.append((-v))
+        freq = {}
+        for ch in tasks:
+            freq[ch] = freq.get(ch, 0)+1
 
-        hq.heapify(h)
+        h = []
+        for k, v in freq.items():
+            hq.heappush(h, (-v, 0, k))
         
         q = deque()
-
-
-        intv = 0
-        while(q or h):
+        time = 0
+        while(h or q):
+            while(q and q[0][1]<=time):
+                hq.heappush(h, q.popleft())
+            
+            while(h and h[0][1]>time):
+                q.append(hq.heappop(h))
+            
             if(h):
-                t = hq.heappop(h)
-                t+=1
-                if(t<0):
-                    q.append((intv+n, t))
+                (count, t, task) = hq.heappop(h)
+                count = -count
+                count-=1
+                if(count>0):
+                    hq.heappush(h, (-count, time + n + 1, task))
             
-            if(q):
-                if(q[0][0]==intv):
-                    tm, t = q.popleft()
-                    hq.heappush(h, t)
-            
-            intv+=1
-
-        return intv
+            time +=1
+        return time
