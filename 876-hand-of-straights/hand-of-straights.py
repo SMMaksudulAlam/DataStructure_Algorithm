e01da8228baad7fc8b7947f56b5e6afca9bfd54a
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-14 01:55:13 -0700

    Time: 52 ms (17.02%) | Memory: 21.7 MB (8.06%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,28 +1,39 @@
-import heapq as hq
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if(len(hand)%groupSize != 0):
             return False
-        dic = {}
-        for e in hand:
-            dic[e] = dic.get(e, 0)+1
         
-        count = 0
-        while(dic):
-            lst = list(dic.keys())
-            for k in lst:
-                if(k+1 not in dic):
-                    c = 0
-                    while(k in dic):
-                        c+=1
-                        dic[k]-=1
-                        if(dic[k]==0):
-                            del dic[k]
-                        if(c == groupSize):
-                            count += 1
-                            break
-                        k-=1
-            #print(count, dic)
-        return count == len(hand)//groupSize
+        expected_group_count = len(hand)//groupSize
 
-            
+        freq = {}
+        for e in hand:
+            freq[e] = freq.get(e, 0)+1
+        
+        q = []
+        for k, v in freq.items():
+            q.append([k, v])
+        
+        q.sort(key = lambda x:x[0])
+        q = deque(q)
+
+        left = 0
+        count = 0
+        while(q):
+            if(len(q)<groupSize):
+                return False
+            mini = math.inf
+            for i in range(groupSize):
+                if(i>0):
+                    if(q[i][0]!=q[i-1][0]+1):
+                        return False
+                mini = min(mini, q[i][1])
+            count += mini
+            if(mini == 0):
+                return False
+            for i in range(groupSize):
+                q[i][1] -= mini
+            while(q and q[0][1]==0):
+                q.popleft()
+        if(count == expected_group_count):
+            return True
+        return False
