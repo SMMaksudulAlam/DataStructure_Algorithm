0a3e4f3840459423c6d03f6cbbc10049cf0865ea
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-14 02:18:51 -0700

    Time: 590 ms (5.02%) | Memory: 21.3 MB (28.85%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -8,32 +8,28 @@ class Solution:
         freq = {}
         for e in hand:
             freq[e] = freq.get(e, 0)+1
-        
-        q = []
-        for k, v in freq.items():
-            q.append([k, v])
-        
-        q.sort(key = lambda x:x[0])
-        q = deque(q)
 
         left = 0
         count = 0
-        while(q):
-            if(len(q)<groupSize):
-                return False
-            mini = math.inf
-            for i in range(groupSize):
-                if(i>0):
-                    if(q[i][0]!=q[i-1][0]+1):
-                        return False
-                mini = min(mini, q[i][1])
-            count += mini
-            if(mini == 0):
-                return False
-            for i in range(groupSize):
-                q[i][1] -= mini
-            while(q and q[0][1]==0):
-                q.popleft()
+        while(freq):
+            keys = list(freq.keys())
+            for k in keys:
+                if(k+1 not in freq):
+                    mini = math.inf
+                    for i in range(groupSize):
+                        ky = k - i
+                        if(ky not in freq):
+                            return False
+                        mini = min(mini, freq[ky])
+
+                    count += mini
+                    for i in range(groupSize):
+                        ky = k - i
+                        freq[ky] -= mini
+                        if(freq[ky]==0):
+                            del freq[ky]
+                    break
+
         if(count == expected_group_count):
             return True
         return False
