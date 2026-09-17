c87941ebf3b0fa39fd4beca8ac3553a41c0f8b9b
SMMaksudulAlam <122957427+SMMaksudulAlam@users.noreply.github.com>
2026-09-17 11:23:46 -0700

    Time: 3 ms (90.89%) | Memory: 20.1 MB (33.97%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,30 +1,31 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        s = "X"+s
-        p = "X"+p
-
-        ans = []
-        row = [False]*len(s)
-        row[0] = True
-        ans.append(row)
-
-        for i in range(1, len(p)):
-            row = [False]*len(s)
-            if(p[i]=='*'):
-                row[0] = ans[-2][0]
-            ans.append(row)
-
-        for i in range(1, len(p)):
-            for j in range(1, len(s)):
-                if(p[i]==s[j]):
-                    ans[i][j] = ans[i-1][j-1]
-                if(p[i]=='.'):
-                    ans[i][j] = ans[i-1][j-1]
-                elif(p[i]=='*'):
-                    #print(i, j, p[i], s[j])
-                    ans[i][j] = ans[i-2][j]
-                    if(p[i-1] =='.' or p[i-1] == s[j]):
-                        ans[i][j] = ans[i][j] or ans[i][j-1]
-                else:
-                    pass
-        return ans[-1][-1]
+        dp = {}
+        def match(ind_s, ind_p):
+            if((ind_s, ind_p) in dp):
+                return dp[(ind_s, ind_p)]
+            if(ind_s<0):
+                if(ind_p<0):
+                    return True
+                if(ind_p%2==1):
+                    for x in range(0, ind_p, 2):
+                        if(p[x+1]!='*'):
+                            return False
+                    return True
+                return False
+            if(ind_p<0):
+                return False
+            
+            if(p[ind_p] == s[ind_s] or p[ind_p] == '.'):
+                dp[(ind_s, ind_p)] =  match(ind_s-1, ind_p-1)
+            elif(p[ind_p] == '*'):
+                dir1 = match(ind_s, ind_p-2)
+                dir2 = False
+                if(ind_p>0 and (p[ind_p-1] == '.' or s[ind_s] == p[ind_p-1])):
+                    dir2 = match(ind_s-1, ind_p)
+                dp[(ind_s, ind_p)] = dir1 or dir2
+            else:
+                dp[(ind_s, ind_p)] = False
+            return dp[(ind_s, ind_p)]
+        
+        return match(len(s)-1, len(p)-1)
